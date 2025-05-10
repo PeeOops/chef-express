aa03b3a36c4d9229df4659b7db96b0a57cb1c004
PeeOops <liem.richiee@gmail.com>
2025-05-10 15:32:20 +0700

    Added navbar ui

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,15 @@
 const Navigation = () => {
     return(
-        <>
-
-        </>
+        <div className="flex flex-row place-content-between items-center font-bold text-lg bg-amber-400 px-24 py-2">
+            <ul className="flex flex-row gap-6 items-center">
+                <li className="cursor-pointer">Home</li>
+                <li className="cursor-pointer">Favourites</li>
+            </ul>
+            <div className="flex flex-row gap-2 items-center">
+                <input type="text" className="focus:outline-none border-b-1 p-1" placeholder="Search recipes..." />
+                <button className="cursor-pointer">Search</button>
+            </div>
+        </div>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
+import Navigation from "../components/Navigation";
+
 const Home = () => {
     return(
         <>
+            {/* Title */}
             <div className="flex flex-col justify-center items-center my-4">
                 <h1 className="text-4xl font-bold">ChefExpress</h1>
                 <p className="text-lg">Explore - Cook - Enjoy</p>
             </div>
+            {/* Navbar */}
+            <Navigation />
+            {/* Hero */}
         </>
     )
 }
