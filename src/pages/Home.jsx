24540510add22e15003c1401f5305613753017da
PeeOops <liem.richiee@gmail.com>
2025-05-14 11:32:57 +0700

    Added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Navigation from "../components/Navigation";
+import Hero from "../assets/images/hero.jpg";
 
 const Home = () => {
     return(
@@ -11,6 +12,16 @@ const Home = () => {
             {/* Navbar */}
             <Navigation />
             {/* Hero */}
+            <section className="relative h-[400px] w-full overflow-hidden">
+                <img
+                    src={Hero}
+                    alt="Hero image"
+                    className="absolute inset-0 h-full w-full object-cover"
+                />
+                <div className="relative z-10 flex items-center justify-center h-full text-white bg-black/50">
+                    <h1 className="text-4xl font-bold">Your next meal starts here</h1>
+                </div>
+            </section>
         </>
     )
 }
