1a101bff5c40bacb615503048b4626897b901f97
PeeOops <liem.richiee@gmail.com>
2025-05-16 15:37:08 +0700

    Added 404 page

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/index.css'
 import Home from './pages/Home.jsx'
 import Favourites from './pages/Favourites.jsx';
+import {PageNotFound} from './components/404.jsx';
 
 
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
+      <Route path="*" element={<PageNotFound />} />
       <Route path="/" element={<Home/>} />
       <Route path="/favourite" element={<Favourites/>} />
     </Routes>
