e74aba3ca8c23c0e8a92d52668fe635be7410b1b
PeeOops <liem.richiee@gmail.com>
2025-05-21 13:25:23 +0700

    Fixed fontAwesomes dependency

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,9 +1,15 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const FoodDetails = () => {
     return(
         <>
             <div className="flex flex-col px-28 mt-6">
-                <p className="font-bold text-lg">Back</p>
+                <div className="flex flex-row items-center gap-2 cursor-pointer hover:text-amber-600">
+                    <FontAwesomeIcon icon={faArrowLeft} />
+                    <p className="font-bold text-lg">Back</p>
+                </div>
+                
                 {/* Header */}
                 <div className="flex flex-row gap-8 mt-6">
                     <img src="https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg" alt="" className="w-84 rounded-md" />
