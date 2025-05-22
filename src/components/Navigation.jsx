632df1a196157e1d221b030127388bab235f0e9e
PeeOops <liem.richiee@gmail.com>
2025-05-22 11:45:42 +0700

    Added responsivenes to navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,9 +8,10 @@ const Navigation = () => {
                 <h1 className="text-4xl font-bold">ChefExpress</h1>
                 <p className="text-lg">Explore - Cook - Enjoy</p>
             </div>
-            <div className="flex flex-row place-content-between items-center font-bold text-lg bg-amber-600 shadow-gray-600 shadow-md px-28 py-2">
-                <ul className="flex flex-row gap-6 items-center">
+            <div className="font-bold text-lg bg-amber-600 shadow-gray-600 shadow-md px-6 md:px-28 py-2">
+                <ul className="flex flex-col md:flex-row gap-2 md:gap-6 items-start md:items-center">
                     <Link to="/?category=Beef&page=1" className={`cursor-pointer ${location.pathname === "/" ? "text-white" : "text-black" }`}>Home</Link>
+                    <span className="border-1 md:border-0 w-full md:w-auto"></span>
                     <Link to="/favourite" className={`cursor-pointer ${location.pathname === "/favourite" ? "text-white" : "text-black" }`}>Favourites</Link>
                 </ul>
             </div>
