256e9230afdf3f60eb041736a025217fba2618ae
PeeOops <liem.richiee@gmail.com>
2025-05-26 15:40:36 +0700

    Added view on saved recipes

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -1,20 +1,23 @@
 import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
+import { Link } from "react-router-dom";
 
 const Favourites = () => {
 
     const [savedRecipe, setSavedRecipe] = useState([]);
 
+
+    // Show saved recipe lists
     useEffect(() => {
         const savedLists = JSON.parse(localStorage.getItem("recipes") || "[]");
 
         setSavedRecipe(savedLists);
     },[])
 
+    // Delete saved recipe
     const handleClickDelete = (id) => {
         const updated = savedRecipe.filter(recipe => recipe.id !== id);
-
         localStorage.setItem("recipes", JSON.stringify(updated));
         setSavedRecipe(updated);
     };
@@ -49,8 +52,8 @@ const Favourites = () => {
                                 <p className="text-lg"><span className="font-bold">Tags:</span> {recipe?.tags ?? "-"}</p>
                             </div>
                             <div className="flex flex-row gap-2">
-                                <button className="text-black p-1 font-bold rounded-sm cursor-pointer hover:text-white hover:bg-amber-600 
-                                active:text-white active:bg-amber-600 border-2 border-black w-fit">View Recipe</button>
+                                <Link to={`/meals/${recipe.id}`} className="text-black p-1 font-bold rounded-sm cursor-pointer hover:text-white hover:bg-amber-600 
+                                active:text-white active:bg-amber-600 border-2 border-black w-fit">View Recipe</Link>
                                 <button onClick={() => handleClickDelete(recipe.id)}className="text-black p-1 font-bold rounded-sm cursor-pointer hover:text-white hover:bg-amber-600 
                                 active:text-white active:bg-amber-600 border-2 border-black w-fit">Delete</button>
                             </div>                    
