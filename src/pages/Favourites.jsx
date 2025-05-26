9108ccafac78a6f605ab8b70fcefbb8dddca5b15
PeeOops <liem.richiee@gmail.com>
2025-05-26 15:32:16 +0700

    Added delete saved recipe on favourite page

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -12,13 +12,32 @@ const Favourites = () => {
         setSavedRecipe(savedLists);
     },[])
 
+    const handleClickDelete = (id) => {
+        const updated = savedRecipe.filter(recipe => recipe.id !== id);
+
+        localStorage.setItem("recipes", JSON.stringify(updated));
+        setSavedRecipe(updated);
+    };
+
     return(
         <>
             {/* Navbar */}
             <Navigation />
             
             {/* Content */}
-            {
+            {   
+                savedRecipe.length <= 0 ? 
+                    <div className="grid min-h-full place-items-centerpx-6 py-24 sm:py-32 lg:px-8">
+                        <div className="text-center">
+                        <h1 className="mt-4 text-3xl font-semibold tracking-tight text-balance text-red-600 sm:text-7xl">
+                            Saved recipe not found
+                        </h1>
+                        <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+                            Add one!
+                        </p>
+                        </div>
+                    </div>
+                :
                 savedRecipe.map((recipe) => (
                     <div key={recipe.id} role="button" className="flex flex-col md:flex-row gap-6 mx-4 md:mx-28 p-8 mt-6 bg-white rounded-md shadow-gray-600 shadow-md">
                         <img src={recipe.img} alt={recipe.title} className="w-full md:w-72 mx-auto rounded-md" />
@@ -32,7 +51,7 @@ const Favourites = () => {
                             <div className="flex flex-row gap-2">
                                 <button className="text-black p-1 font-bold rounded-sm cursor-pointer hover:text-white hover:bg-amber-600 
                                 active:text-white active:bg-amber-600 border-2 border-black w-fit">View Recipe</button>
-                                <button className="text-black p-1 font-bold rounded-sm cursor-pointer hover:text-white hover:bg-amber-600 
+                                <button onClick={() => handleClickDelete(recipe.id)}className="text-black p-1 font-bold rounded-sm cursor-pointer hover:text-white hover:bg-amber-600 
                                 active:text-white active:bg-amber-600 border-2 border-black w-fit">Delete</button>
                             </div>                    
                         </div>
